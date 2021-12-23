1e917fc3b559aa281b23ba526f18f9bc2830ca37
unknown <turudija007@gmail.com>
2021-12-23 20:27:32 +0100

    Improved videos source

--- pages/weatherPage.vue
+++ pages/weatherPage.vue
@@ -2,25 +2,25 @@
     <div class="w-full h-[100vh] relative">
         <div id="responsiveVideoWrapper" class="absolute inset-0  -z-50 brightness-[.3]">
             <video autoplay muted loop id="myVideo" class="h-full w-full object-cover" :class="getWeatherType=='Clouds' ? '' : 'hidden'">
-                <source src="_nuxt/assets/videos/Clouds.mp4" type="video/mp4">
+                <source src="../assets/videos/Clouds.mp4" type="video/mp4">
             </video>
             <video autoplay muted loop id="myVideo" class="h-full w-full object-cover" :class="getWeatherType=='Clear' ? '' : 'hidden'">
-                <source src="_nuxt/assets/videos/Clear.mp4" type="video/mp4">
+                <source src="../assets/videos/Clear.mp4" type="video/mp4">
             </video>
             <video autoplay muted loop id="myVideo" class="h-full w-full object-cover" :class="getWeatherType=='Rain' ? '' : 'hidden'">
-                <source src="_nuxt/assets/videos/Rain.mp4" type="video/mp4">
+                <source src="../assets/videos/Rain.mp4" type="video/mp4">
             </video>
             <video autoplay muted loop id="myVideo" class="h-full w-full object-cover" :class="getWeatherType=='Snow' ? '' : 'hidden'">
-                <source src="_nuxt/assets/videos/Snow.mp4" type="video/mp4">
+                <source src="../assets/videos/Snow.mp4" type="video/mp4">
             </video>
             <video autoplay muted loop id="myVideo" class="h-full w-full object-cover" :class="getWeatherType=='Thunderstorm' ? '' : 'hidden'">
-                <source src="_nuxt/assets/videos/Thunderstorm.mp4" type="video/mp4">
+                <source src="../assets/videos/Thunderstorm.mp4" type="video/mp4">
             </video>
             <video autoplay muted loop id="myVideo" class="h-full w-full object-cover" :class="getWeatherType=='Drizzle' ? '' : 'hidden'">
-                <source src="_nuxt/assets/videos/Drizzle.mp4" type="video/mp4">
+                <source src="../assets/videos/Drizzle.mp4" type="video/mp4">
             </video>
             <video autoplay muted loop id="myVideo" class="h-full w-full object-cover" :class="getWeatherType=='Haze' ? '' : 'hidden'">
-                <source src="_nuxt/assets/videos/Haze.mp4" type="video/mp4">
+                <source src="../assets/videos/Haze.mp4" type="video/mp4">
             </video>
         </div>
         <div class="flex flex-col justify-center items-center">
