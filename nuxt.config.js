6a4ad11dfec180cc772ab4302f5e5c40d5dcdac4
Nikola <turudija007@gmail.com>
2021-12-29 10:04:56 +0100

    Changed name from Universal App to Weather App

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Universal App',
+    title: 'Weather App',
     htmlAttrs: {
       lang: 'en'
     },
