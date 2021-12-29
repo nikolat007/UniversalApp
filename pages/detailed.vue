7cd4eb3d2400a0b0212420121d58ed4e310ae0c1
Nikola <turudija007@gmail.com>
2021-12-29 10:23:56 +0100

    Added midddleware protection on /detailed page

--- pages/detailed.vue
+++ pages/detailed.vue
@@ -18,6 +18,16 @@ export default {
         MoleculeWeatherVideos,
         MoleculeNavigation,
         OrganismDetailedWeather
+    },
+    middleware({ store, redirect }) {
+        if(!store.getters['weather/getWeather']){
+            return redirect('/')
+        }
+    },
+    computed: {
+        getWeatherData() {
+            return this.$store.getters['weather/getWeather'];
+        }
     }
 }
 </script>
